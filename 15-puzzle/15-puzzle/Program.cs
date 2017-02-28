6d36468ac4ebd023744baeaa6004b69a65597cee
Ilya <boss.malinovskiy@mail.ru>
2017-02-28 16:56:20 +0500

    Add files via upload

--- 15-puzzle/15-puzzle/Program.cs
+++ 15-puzzle/15-puzzle/Program.cs
@@ -1,36 +1,33 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
+
 namespace _15_puzzle
 {
     class Program
     {
+        
         static void Main(string[] args)
         {
-            //int[] numberedSquare = Array.;
-            // ;
-            var pi = new Puzzle(1, 2, 3, 4, 5, 6, 7, 8,9,10,11,12,13,14,15, 0);
-            pi.Shift(6);
-            Printer.PrintPuzzle(pi);
-            Console.WriteLine(pi[1, 1]);
-//            if (Math.Sqrt(numberedSquare.Count()) != Math.Truncate(Math.Sqrt(numberedSquare.Count()))
-//                    || Math.Sqrt(numberedSquare.Count()) == 1)
-//            {
-//                throw new ArgumentException("Неверное число параметров");
-//            }
-//            else
-//            {
-//                }
-//            }
-//        }
-//            catch (ArgumentException e)
-//            {
-//                Console.WriteLine(e.Message);
-//                System.Environment.Exit(1);
-//            }
+            int[] numberedSquare = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 0 };
+            try 
+            {
+                Line.Check(numberedSquare);
+                var game = new Puzzle(numberedSquare);
+                game.Shift(6);
+                Printer.PrintPuzzle(game);
+            }
+            catch (ArgumentException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            Printer.PrintPuzzle(Puzzle.FromCSV("input.csv"));
+             
         }
      }
 }
--- 15-puzzle/15-puzzle/Puzzle.cs
+++ 15-puzzle/15-puzzle/Puzzle.cs
@@ -3,52 +3,39 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace _15_puzzle
 {
     class Puzzle
     {
-        private List<List<int>> frame;
+        private int[,] frame;
         public readonly int SizeOfFrame;
         private Dictionary<int, Position> positions;
         public Puzzle(params int[] numberedSquare)
         {
-            try
+            positions = new Dictionary<int,Position>();
+            SizeOfFrame = Convert.ToInt32(Math.Sqrt(numberedSquare.Count()));
+            frame = new int[SizeOfFrame, SizeOfFrame];
+            for (int i = 0; i < SizeOfFrame; i++)
             {
-                if (Math.Sqrt(numberedSquare.Count()) != Math.Truncate(Math.Sqrt(numberedSquare.Count())) 
-                    || Math.Sqrt(numberedSquare.Count()) == 1)
+                for (int j = 0; j < SizeOfFrame; j++)
                 {
-                    throw new ArgumentException("Неверное число параметров");
-                }
-                else
-                {
-                    positions = new Dictionary<int,Position>();
-                    SizeOfFrame = Convert.ToInt32(Math.Sqrt(numberedSquare.Count()));
-                    frame = new List<List<int>>();
-                    for (int i = 0; i < SizeOfFrame; i++)
-                    {
-                        frame.Add(new List<int>());
-                        for (int j = 0; j < SizeOfFrame; j++)
-                        {
-                            int value = Convert.ToInt32(numberedSquare[i * SizeOfFrame + j]);
-                            frame[i].Add(value);
-                            positions.Add(value, new Position(i, j));
-                        }
-                    }
+                    int value = Convert.ToInt32(numberedSquare[i * SizeOfFrame + j]);
+                    frame[i,j] = value;
+                    positions.Add(value, new Position(i, j));
                 }
             }
-            catch (ArgumentException e)
-            {
-                Console.WriteLine(e.Message);
-                System.Environment.Exit(1);
-            }
-            
         }
         public int this[int x, int y]
         {
             get
             {
-                return frame[x][y];
+                return frame[x,y];
+            }
+            set
+            {
+                frame[x,y] = value;
             }
         }
         public Position GetLocation(int value)
@@ -62,8 +49,8 @@ namespace _15_puzzle
                 if (positions[value] - positions[0] == 1)
                 {
                     Position positionZero = positions[0];
-                    frame[positions[0].X][positions[0].Y] = value;
-                    frame[positions[value].X][positions[value].Y] = 0;
+                    this[positions[0].X , positions[0].Y] = value;
+                    this[positions[value].X , positions[value].Y] = 0;
                     positions[0] = positions[value];
                     positions[value] = positionZero;
                 }
@@ -73,6 +60,20 @@ namespace _15_puzzle
             {
                 Console.WriteLine(e.Message);
             }
+        }
+        public static Puzzle FromCSV(string file)
+        {
+            string[] data = File.ReadAllLines(file);
+            List<int> convertedData = new List<int>();
+            for (int i = 0; i < data.Count(); i++)
+            {
+                for (int j = 0; j < data[i].Split(';').Count(); j++)
+                {
+                    convertedData.Add(Convert.ToInt32(data[i].Split(';')[j]));
+                }
+            }
+            Puzzle game = new Puzzle(convertedData.ToArray<int>());
+            return game;
         }
     }
 }
